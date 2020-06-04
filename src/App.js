7653d7ced94f7d2863e1b922f0de4db93b8fc2b3
Jack Graham <graham.jm@protonmail.com>
2020-06-04 12:31:31 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
 
 function App() {
+
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <GoogleLogin
+            clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+            buttonText="Login"
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+        </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
